1b03fbbeb611b150b1c8e2036b3c8aa0c3ae8794
Travis Briggs <audiodude@gmail.com>
2019-05-21 19:56:40 +0000

    Google sign in handling, plus stub for new page creation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import pip
 import os
 import pathlib
@@ -6,13 +7,27 @@ import sys
 import time
 import venv
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
 import flask
+from flask_session import Session
 import pymongo
 
 client = pymongo.MongoClient(connect=False)
 emperor_db = client.emperor
+rainfall_db = client.rainfall
+
+CLIENT_ID = os.environ['RAINFALL_CLIENT_ID']
 
 app = flask.Flask(__name__)
+app.config.update({
+  'SESSION_TYPE': 'mongodb',
+  'SESSION_MONGODB_DB': 'rainfall',
+  'SESSION_COOKIE_SECURE': False,
+  'SESSION_USE_SIGNER': True,
+  'SECRET_KEY': os.environ['FLASK_SECRET'],
+  'PERMANENT_SESSION_LIFETIME': timedelta(days=90),
+})
 app.debug = True
 
 def clone_repo(name):
@@ -50,6 +65,7 @@ gid = www-data
 wsgi-file = /var/data/%(start_char)s/%(name)s/sitebuilder.py
 plugin = python
 callable = app
+env = RAINFALL_USERNAME=%s(name)
 ''' % {'start_char': start_char, 'name': name},
     "ts" : time.gmtime(),
     "socket" : "/var/run/uwsgi/%s.socket" % name,
@@ -76,7 +92,46 @@ def update_nginx(name):
 
 @app.route('/')
 def index():
+  if flask.session.get('user_id'):
+    return flask.redirect('/edit')
+
   return flask.render_template('index.html')
+
+@app.route('/tokensignin', methods=['POST'])
+def tokensignin():
+  token = flask.request.form['id_token']
+  try:
+    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+    if idinfo['iss'] not in (
+        'accounts.google.com', 'https://accounts.google.com'):
+      raise ValueError('Wrong issuer.')
+
+    if idinfo['aud'] != CLIENT_ID:
+      raise ValueError('Wrong client.')
+
+    user_id = idinfo['sub']
+    flask.session['user_id'] = user_id
+    return ('', 201)
+  except ValueError:
+    # Invalid token
+    return ('Sign in error', 403)
+
+@app.route('/edit')
+def edit():
+  user_id = flask.session.get('user_id')
+  if not user_id:
+    return flask.redirect('/')
+
+  site = rainfall_db.sites.find_one({'user_id': user_id})
+  if not site:
+    return flask.redirect('/new')
+
+  return flask.render_template('edit.html', site=site)
+
+@app.route('/new')
+def new():
+  return flask.render_template('new.html')
 
 @app.route('/create', methods=['POST'])
 def create():
